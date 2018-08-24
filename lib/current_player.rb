80e7380540d13dc39879ee688fa8c8c888aca6bf
valentine-m@live.fr <valentine-m@live.fr>
2018-08-24 21:11:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def turn_count(board)
   counter = 0 
   board.each do |turn|
-    puts "This is turn #{counter}"
+    puts "This is turn number #{counter}"
     counter += 1
   end
 end 
